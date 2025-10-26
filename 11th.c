5fe9100da013786e8c8ef2d8f4b43d66f36e3f61
prasantakumarswain1000 <prasantakumarswain1000@gmail.com>
2025-10-27 00:16:03 +0530

    Add files via upload

--- 11th.c
+++ 11th.c
@@ -1,12 +1,68 @@
-//facctorial
-int main(){
-    int n;
-    printf("enter number");
-    scanf("%d",&n);
-    int fact=1;
-    for(int i=1;i<=n;i++){
-        fact=fact*i;
+#include <stdio.h>
+#define N 10
+int main() {
+    // Write C code here
+
+    // Write C code here
+int a[N][N],b[N][N],c[N][N],i,j,k,sum,m,n,p,q;
+printf("enter the num of rows and columns in first matrix:\n");
+scanf("%d%d",&m,&n);
+printf("enter the first matrix:\n");
+for(i=0;i<m;i++)
+{
+    for(j=0;j<n;j++)
+    {
+        scanf("%d",&a[i][j]);
     }
-    printf("final factorial id%d",fact);
-    return 0;
+}
+printf("enter a rows and columns for second matrix:\n");
+scanf("%d%d",&p,&q);
+printf("\n enter second matrix\n");
+for(i=0;i<p;i++){
+    for(j=0;i<q;j++)
+    {
+        scanf("%d",&b[i][j]);
+    }
+}
+printf("\n first matrix is :\n");
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("/nsecond matrix is:\n");
+    for(i=0;i<p;i++)
+    {
+        for(j=0;j<q;j++)
+        {
+            printf("%d\t",b[i][j]);
+        }
+        printf("\n");
+    }
+    if(n!=p){
+        printf("can not multiply");
+    }
+    else{
+        for(i=0;i<m;i++)
+        {
+            for(j=0;j<q;j++){
+                sum=0;
+                for(k=0;k<m;k++){
+                    sum=sum+(a[i][k]*b[k][j]);
+                }
+                c[i][j]=sum;
+            }
+        }
+        printf("multiplication is:\n");
+        for(i=0;i<m;i++){
+            for(j=0;j<q;j++){
+                printf("%d\t",c[i][j]);
+            }
+            printf("\n");
+        }
+    }
+
 }
