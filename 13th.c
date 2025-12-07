2280e6b121f565d47846dd82da3f72406563fb7e
prasantakumarswain1000 <prasantakumarswain1000@gmail.com>
2025-12-08 00:26:13 +0530

    Add files via upload

---
+++ 13th.c
@@ -0,0 +1,18 @@
+// 4*3 matrix
+#include <stdio.h>
+
+  void main() {
+      int a[4][3],i,j;
+      printf("enter  the value in row wise ");
+      for(i=0;i<4;i++){
+          for(j=0;j<3;j++){
+              scanf(" %d",&a[i][j]);
+      }}
+        printf("enter  the value in column wise ");
+      for(i=0;i<4;i++){
+          for(j=0;j<3;j++){
+              printf("  %d",&a[i][j]);
+      }}
+      
+   
+}
